0a426d261c0b62df0bd919eb6f0dc68da9546f53
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-12 16:34:33 +0900

    chore : add User entity

---
+++ src/main/java/com/flab/jbly/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.flab.jbly.domain.user;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+  private Long id;
+
+  private String userId;
+
+  private String password;
+
+  private String name;
+
+  private String phone;
+
+  private String email;
+
+  private String address;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+}
+
