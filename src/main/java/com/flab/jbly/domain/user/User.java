136a1db37a935ce75ed27af37d3d05b7497574e7
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-12 16:46:02 +0900

    chore : dependencies add

--- src/main/java/com/flab/jbly/domain/user/User.java
+++ src/main/java/com/flab/jbly/domain/user/User.java
@@ -9,22 +9,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-  private Long id;
+		private Long id;
 
-  private String userId;
+		private String userId;
 
-  private String password;
+		private String password;
 
-  private String name;
+		private String name;
 
-  private String phone;
+		private String phone;
 
-  private String email;
+		private String email;
 
-  private String address;
+		private String address;
 
-  private LocalDateTime createdAt;
+		private LocalDateTime createdAt;
 
-  private LocalDateTime updatedAt;
+		private LocalDateTime updatedAt;
 }
 
