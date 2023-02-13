e170193e8b31153842656bc0eb6c9f56b518f92f
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-13 17:16:52 +0900

    chore : add seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO user(user_id, userId, password, name, phone, email, address, created_at, updated_at)
+VALUES (1, 'Steve', '123', '연수', '01000000000', 'lee@naver.com', 'Seoul', now(), null),
+       (2, 'lime', '456', '소정', '01000000001', 'kim@naver.com', 'Busan', now(), null),
+       (3, 'cookie', '789', '소유', '01000000002', 'choi@naver.com', 'Ulsan', now(), null),
+       (4, 'tree', '133', '다혜', '01000000003', 'park@naver.com', 'Seoul', now(), null),
+       (5, 'bowl', '434', '은혁', '01000000004', 'jung@naver.com', 'Daegu', now(), null);
+
