4ea01ab471f01e542e895e43ad9fc5a3bf256b6c
Sojeong Park <rty5262@naver.com>
2023-02-18 16:55:18 +0900

    Revert "[#21] Github Action을 이용한 CI환경 구축"

---
+++ src/test/java/com/flab/jbly/JblyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.flab.jbly;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
