7a03219511d9eb1caacfe57b7700373f9bd0bebc
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-25 15:50:52 +0900

    refactor : morecherry crawling data formatting

--- crawling/parsing/MoreCherrySiteCrawling.py
+++ crawling/parsing/MoreCherrySiteCrawling.py
@@ -1,46 +1,81 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def getDatas():
-    urls = []
+def getTotalItemList():
+    # WebDriver를 초기화합니다.
+    driver = webdriver.Chrome()
+
+    # Var Setting
+    storeName = "morecherry"
     result = []
-    storeName = "morecherry" # 5개의 필드
-    urls.append("https://m.more-cherry.com/category/outwear/24") # outwear
-    urls.append("https://m.more-cherry.com/category/top/25/") # top
-    urls.append("https://m.more-cherry.com/category/pants/26/") # pants
-    urls.append("https://m.more-cherry.com/category/accessory/28/") # acc
-    urls.append("https://m.more-cherry.com/product/list_thumb.html?cate_no=42") # shoes
+    urls = []
+    urls.append("https://m.more-cherry.com/category/outwear/24")  # outwear
+    urls.append("https://m.more-cherry.com/category/top/25")  # top
+    urls.append("https://m.more-cherry.com/category/pants/26")  # pants
+    urls.append("https://m.more-cherry.com/category/accessory/28")  # acc
+    urls.append("https://m.more-cherry.com/product/list_thumb.html?cate_no=42")  # shoes
 
-    splitedUrl = urls[0].split("/")
-    itemType = splitedUrl[-2]
-    html = urlopen(urls[0])
-    soup = BeautifulSoup(html, 'html.parser')
+    for url in urls:
+        itemType = ""
+        if "category" in url:
+            splitedUrl = url.split("/")
+            itemType = splitedUrl[-2]
+        else:
+            itemType = "shoes"
+        # 웹 페이지를 로드합니다.
+        driver.get(url)
+        element = driver.find_element(by=By.XPATH, value='//*[@id="contents"]/div[4]/a')
 
-    datas = soup.find_all("ul", "prdList grid2") # 상품이 담겨있는 부분의 리스트를 가져옵니다.
-    datas = str(datas) # 문자열로 변경 후
-    datas = datas.split('<li class="xans-record-">') # 각 상품마다 담기는 리스트로 만듭니다.
-    datas = datas[1:]
+        # Get Total Page
+        currentPage = driver.find_element(by=By.XPATH, value='//*[@id="more_current_page"]')
+        totalPage = driver.find_element(by=By.XPATH, value='//*[@id="more_total_page"]')
 
-    for data in datas:
-        targetData = BeautifulSoup(data, 'html.parser')
-        # get Item
-        itemTag = targetData.find('span', {'class': 'name'})
-        itemName = itemTag.text
+        loopingTime = int(totalPage.text) - int(currentPage.text)
 
-        # get Image
-        imageTag = targetData.find('img')
-        url = imageTag['src']
-        getUrl = 'https:' + url
+        for i in range(1):
+            driver.execute_script("arguments[0].click();", element)
+            time.sleep(5)
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        datas = soup.find("ul", "prdList grid2").find_all("li", recursive=False)
+        datas = list(map(str, datas))  # 문자열로 변경 후
+        datas = datas[1:]
+        print("datas = ", len(datas))
+
+        for data in datas:
+            itemInfoGather = []
+            targetData = BeautifulSoup(data, 'html.parser')
+            # get Item
+            itemTag = targetData.find('span', {'class': 'name'})
+            itemName = itemTag.text
+
+            # get Image
+            imageTag = targetData.find('img')
+            url = imageTag['src']
+            getUrl = 'https:' + url
 
 
-        # get Price
-        priceTag = targetData.find('li', {'class': 'price'})
-        getPrice = priceTag.text.replace(',', '')
-        getPrice = getPrice.replace('원','')
+            # get Price
+            priceTag = targetData.find('li', {'class': 'price'})
+            getPrice = priceTag.text.replace(',', '')
+            getPrice = getPrice.replace('원','')
 
-        print(storeName, itemName, getUrl, getPrice, itemType)
-        print("============================================================")
-    return None
+            itemInfoGather.append(storeName)
+            itemInfoGather.append(itemName)
+            itemInfoGather.append(getUrl)
+            itemInfoGather.append(getPrice)
+            itemInfoGather.append(itemType)
+            copyItemInfo = itemInfoGather.copy()
+            result.append(copyItemInfo)
+            itemInfoGather.clear()
+
+    driver.close()
+    return result
