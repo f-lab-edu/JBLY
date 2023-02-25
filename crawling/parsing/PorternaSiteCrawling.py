481be55bcdb978e3a54cf96a27ab41c16b9b5424
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-25 17:41:54 +0900

    feat : end paging when we seee #none herf tag

--- crawling/parsing/PorternaSiteCrawling.py
+++ crawling/parsing/PorternaSiteCrawling.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import ssl
+
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -53,4 +56,18 @@ def getTotalProducts():
     price = getPrice.text
 
     print(storeName, itemName, imageUrl, price, itemType)
+
+    while True:
+        element = driver.find_element(by=By.XPATH, value='//*[@id="contents"]/div[2]/div[4]/a[2]')
+        time.sleep(5)
+
+        if element.is_displayed() and element.is_enabled():
+            try:
+                driver.execute_script("arguments[0].click();", element)
+            except:
+                pass
+        if driver.current_url.endswith("#none"):
+            break
+
+    driver.close()
     return None
