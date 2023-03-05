657c85b610daa90b30a3f2e15c7718ba4870e9a7
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-05 14:33:58 +0900

    feat : type classification according to operating system in WebDriver

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
+import platform
 
 def executor():
+
+  osType = platform.system()
+
+  if osType == "Linux" or osType == "Darwin":
+    return webdriver.Chrome()
+
   return webdriver.Chrome(executable_path='C:\chromedriver.exe')
