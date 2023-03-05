379ee80174a582ebc28d96ca13ecbd5315f6e969
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-05 17:56:54 +0900

    chore : mysql connection info change

--- crawling/main.py
+++ crawling/main.py
@@ -1,21 +1,20 @@
 from parsing import MoreCherrySiteCrawling, PorternaSiteCrawling, TheVerlinSiteCrawling
-# from dbConnection import MysqlConnect
+from dbConnection import MysqlConnect
 if __name__ == '__main__':
     # dataTypes = storeName, itemName, imageUrl, price, itemType shopId
     results = []
 
     # shopId == 1
     porternaProducts = PorternaSiteCrawling.getTotalProducts()
+    MysqlConnect.connect(porternaProducts)
 
-    # MysqlConnect.connect(porternaProducts)
-    #
     # # shopId == 2
-    # moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
-    # MysqlConnect.connect(moreCherryProducts)
+    moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
+    MysqlConnect.connect(moreCherryProducts)
 
     # shopId == 3
-    # theverlinProducts = TheVerlinSiteCrawling.getTotalItemList()
-    # MysqlConnect.connect(theverlinProducts)
+    theverlinProducts = TheVerlinSiteCrawling.getTotalItemList()
+    MysqlConnect.connect(theverlinProducts)
 
 
 
