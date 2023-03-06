d922b84d72c558ef18b14fa223dbfd888ad01bb6
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-06 20:28:59 +0900

    feat : db select query

--- crawling/dbConnection/MysqlConnect.py
+++ crawling/dbConnection/MysqlConnect.py
@@ -1,18 +1,26 @@
 from dbConnection import MysqlConnectionInfo
 
+# todo 책임 분리
 def connect(products):
     # productsData order = storeName, itemName, imageUrl, price, itemType, detailInfo, shopId
     connector = MysqlConnectionInfo.connector()
     cursor = connector.cursor()
 
-    sql = "INSERT INTO product (shopName, productName, image, price, productType, detailInfo shopId) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    insertSql = "INSERT INTO product (shopName, productName, image, price, productType, detailInfo shopId) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    selectSql = "SELECT * FROM product WHERE shopId=%s AND productName=%s"
+
 
     for product in products:
         storeName, itemName, imageUrl, price, type, detailInfo, shopId = product
-        try:
-            cursor.execute(sql, (storeName, itemName, imageUrl, price, type, detailInfo, shopId))
-        except:
-            print(storeName, itemName, imageUrl, price, type, detailInfo, shopId)
+
+        cursor.execute(selectSql, (shopId, itemName))
+        result = cursor.fetchone()
+
+        if not result:
+            try:
+                cursor.execute(insertSql, (storeName, itemName, imageUrl, price, type, detailInfo, shopId))
+            except:
+                print(storeName, itemName, imageUrl, price, type, detailInfo, shopId)
     connector.commit()
     cursor.close()
     connector.close()
--- crawling/main.py
+++ crawling/main.py
@@ -1,20 +1,23 @@
 from parsing import MoreCherrySiteCrawling, PorternaSiteCrawling, TheVerlinSiteCrawling
 from dbConnection import MysqlConnect
+import datetime
+
 if __name__ == '__main__':
     # dataTypes = storeName, itemName, imageUrl, price, itemType, detailInfo shopId
-    results = []
+    # results = []
 
     # shopId == 1
-    porternaProducts = PorternaSiteCrawling.getTotalProducts()
-    MysqlConnect.connect(porternaProducts)
+    # porternaProducts = PorternaSiteCrawling.getTotalProducts()
+    # MysqlConnect.connect(porternaProducts)
 
     # shopId == 2
-    moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
-    MysqlConnect.connect(moreCherryProducts)
+    # moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
+    # MysqlConnect.connect(moreCherryProducts)
 
     # shopId == 3
-    theverlinProducts = TheVerlinSiteCrawling.getTotalItemList()
-    MysqlConnect.connect(theverlinProducts)
-
-
-
+    # theverlinProducts = TheVerlinSiteCrawling.getTotalItemList()
+    # MysqlConnect.connect(theverlinProducts)
+    now = datetime.datetime.now()
+    newTime = now.strftime("%Y-%m-%d %H:%M:%S")
+    print(newTime)
+    MysqlConnect.connect(None)
