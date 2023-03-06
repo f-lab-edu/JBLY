e5dc803fc971f1a63ea842f755d6d1eb627ff80b
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-06 20:49:45 +0900

    refactor : query 책임 분리

--- crawling/dbConnection/MysqlConnect.py
+++ crawling/dbConnection/MysqlConnect.py
@@ -1,30 +1,10 @@
 from dbConnection import MysqlConnectionInfo
 
 # todo 책임 분리
-def connect(products):
+def connect():
     # productsData order = storeName, itemName, imageUrl, price, itemType, detailInfo, shopId
     connector = MysqlConnectionInfo.connector()
-    cursor = connector.cursor()
-
-    insertSql = "INSERT INTO product (shopName, productName, image, price, productType, detailInfo shopId) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    selectSql = "SELECT * FROM product WHERE shopId=%s AND productName=%s"
-
-
-    for product in products:
-        storeName, itemName, imageUrl, price, type, detailInfo, shopId = product
-
-        cursor.execute(selectSql, (shopId, itemName))
-        result = cursor.fetchone()
-
-        if not result:
-            try:
-                cursor.execute(insertSql, (storeName, itemName, imageUrl, price, type, detailInfo, shopId))
-            except:
-                print(storeName, itemName, imageUrl, price, type, detailInfo, shopId)
-    connector.commit()
-    cursor.close()
-    connector.close()
-    return None
+    return connector
 
 def disconnect(connector):
     connector.close()
--- crawling/main.py
+++ crawling/main.py
@@ -1,23 +1,31 @@
 from parsing import MoreCherrySiteCrawling, PorternaSiteCrawling, TheVerlinSiteCrawling
 from dbConnection import MysqlConnect
+from dbConnection import ProductQuery
 import datetime
 
 if __name__ == '__main__':
     # dataTypes = storeName, itemName, imageUrl, price, itemType, detailInfo shopId
     # results = []
 
+    connectDB = MysqlConnect.connect()
     # shopId == 1
     # porternaProducts = PorternaSiteCrawling.getTotalProducts()
-    # MysqlConnect.connect(porternaProducts)
+    # porternaInsertData = ProductQuery.checkDuplicatedProducts(connectDB, porternaProducts)
+    # ProductQuery.insertProducts(connectDB, porternaInsertData)
 
     # shopId == 2
     # moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
-    # MysqlConnect.connect(moreCherryProducts)
+    # moreCherryInsertData = ProductQuery.checkDuplicatedProducts(connectDB, moreCherryProducts)
+    # ProductQuery.insertProducts(connectDB, moreCherryInsertData)
 
     # shopId == 3
     # theverlinProducts = TheVerlinSiteCrawling.getTotalItemList()
-    # MysqlConnect.connect(theverlinProducts)
+    # theverlinInsertData = ProductQuery.checkDuplicatedProducts(connectDB, theverlinProducts)
+    # ProductQuery.insertProducts(connectDB, theverlinInsertData)
+
     now = datetime.datetime.now()
     newTime = now.strftime("%Y-%m-%d %H:%M:%S")
     print(newTime)
-    MysqlConnect.connect(None)
+
+    MysqlConnect.disconnect(connectDB) # DB disconnect
+
