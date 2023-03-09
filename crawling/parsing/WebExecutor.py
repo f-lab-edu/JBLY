58036ba8a7a5cebb975fa06f089c8c466e9befe5
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 19:36:45 +0900

    chore : add options before webdriver

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 def executor():
 
+
+  options = Options()
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
   chromeDriverPath = "./chromedriver"
   return webdriver.Chrome(executable_path=chromeDriverPath)
 
