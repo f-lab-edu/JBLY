2c5c13372eae68f9fa6ec07fd0dd338d7367fa05
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 20:10:49 +0900

    chore : give authority to chromedriver

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 def executor():
 
   chromeOptions = Options()
   chromeOptions.add_argument('--no-sandbox')
-  chromeOptions.add_argument('--disable-dev-shm-usage')
   chromedriver = "crawling/parsing/chromedriver"
   return webdriver.Chrome(executable_path=chromedriver, options=chromeOptions)
 
