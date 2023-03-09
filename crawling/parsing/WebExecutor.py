047210e4535187b9118fd1e801105b58e73900ed
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 19:29:22 +0900

    chore : download chrome engine exe file

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -4,6 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def executor():
 
-  chrome_options = webdriver.ChromeOptions()
-  return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+  chromeDriverPath = "./chromedriver"
+  return webdriver.Chrome(executable_path=chromeDriverPath)
 
