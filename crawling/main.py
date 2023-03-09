cfde859f93f334c8b33927c6a6742df5975b95cf
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 21:33:15 +0900

    test : crawling item

--- crawling/main.py
+++ crawling/main.py
@@ -6,28 +6,26 @@ if __name__ == '__main__':
     # dataTypes = storeName, itemName, imageUrl, price, itemType, detailInfo shopId
 
     connectDB = MysqlConnect.connect()
-    driver = WebExecutor.executor()
-    detailDriver = WebExecutor.executor()
 
     # shopId == 1
-    porternaProducts = PorternaSiteCrawling.getTotalProducts(driver, detailDriver)
-    porternaInsertData = ProductQuery.checkDuplicatedProducts(connectDB, porternaProducts)
-    ProductQuery.insertProducts(connectDB, porternaInsertData)
-
+    # porternaProducts = PorternaSiteCrawling.getTotalProducts()
+    # print("4")
+    # porternaInsertData = ProductQuery.checkDuplicatedProducts(connectDB, porternaProducts)
+    # print("5")
+    # ProductQuery.insertProducts(connectDB, porternaInsertData)
+    # print("6")
     # shopId == 2
-    moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts(driver, detailDriver)
+    moreCherryProducts = MoreCherrySiteCrawling.getTotalProducts()
     moreCherryInsertData = ProductQuery.checkDuplicatedProducts(connectDB, moreCherryProducts)
     ProductQuery.insertProducts(connectDB, moreCherryInsertData)
 
     # shopId == 3
-    theverlinProducts = TheVerlinSiteCrawling.getTotalItemList(driver, detailDriver)
-    theverlinInsertData = ProductQuery.checkDuplicatedProducts(connectDB, theverlinProducts)
-    ProductQuery.insertProducts(connectDB, theverlinInsertData)
+    # theverlinProducts = TheVerlinSiteCrawling.getTotalItemList(driver, detailDriver)
+    # theverlinInsertData = ProductQuery.checkDuplicatedProducts(connectDB, theverlinProducts)
+    # ProductQuery.insertProducts(connectDB, theverlinInsertData)
 
     now = datetime.datetime.now()
     newTime = now.strftime("%Y-%m-%d %H:%M:%S")
     print(newTime)
 
-    driver.close()
-    detailDriver.close()
     MysqlConnect.disconnect(connectDB)  # DB disconnect
