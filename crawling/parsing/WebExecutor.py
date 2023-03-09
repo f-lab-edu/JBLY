b88590d8e3f668ee69b74dc6bcf4f49b156657f4
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 21:35:38 +0900

    chore : change web engine setting

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -2,11 +2,10 @@ from selenium import webdriver
 
 def executor():
 
-  # chrome_options = webdriver.ChromeOptions()
-  # chrome_options.add_argument('--headless')
-  # chrome_options.add_argument('--no-sandbox')
-  # chrome_options.add_argument('--disable-dev-shm-usage')
-  # chromedriver = "./chromedriver"
-  # executable_path=chromedriver, options=chrome_options
-  return webdriver.Chrome()
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chromedriver = "./chromedriver"
+  return webdriver.Chrome(executable_path=chromedriver, options=chrome_options)
 
