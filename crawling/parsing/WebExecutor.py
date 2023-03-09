cc5fbcb03381d11cfa8fc849efdd68ce3aefe6df
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-09 19:44:19 +0900

    chore : change webdriver options

--- crawling/parsing/WebExecutor.py
+++ crawling/parsing/WebExecutor.py
@@ -7,6 +7,6 @@ def executor():
   chromeOptions = Options()
   chromeOptions.add_argument('--no-sandbox')
   chromeOptions.add_argument('--disable-dev-shm-usage')
-  chromeOptions.binary_location = "./chromedriver"
-  return webdriver.Chrome(executable_path=chromeOptions)
+  chromedriver = "./chromedriver"
+  return webdriver.Chrome(executable_path=chromedriver, options=chromeOptions)
 
