529222d770f3cb04340cfd4e6e342f3f8bb4c169
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-13 19:49:36 +0900

    chore : change db connection info

--- crawling/dbConnection/CloudDBConnectionInfo.py
+++ crawling/dbConnection/CloudDBConnectionInfo.py
@@ -2,24 +2,16 @@ import pymysql
 import os
 
 def connector():
-    # cloudHost = os.environ.get("DB_HOST")
-    # cloudUser = os.environ.get("DB_USER")
-    # cloudPwd = os.environ.get("DB_PWD")
-    # cloudName = os.environ.get("DB_NAME")
-    # cloudPort = os.environ.get("DB_PORT")
-    #
-    # cloudPort = int(cloudPort)
-    # return pymysql.connect(host=cloudHost,
-    #                        user=cloudUser,
-    #                        password=cloudPwd,
-    #                        db=cloudName,
-    #                        charset='utf8',
-    #                        port=cloudPort)
-    return pymysql.connect(
-        host="db-fhu0d.pub-cdb.ntruss.com",
-        user="jbly_crawling",
-        password="project1!",
-        db="jbly",
-        charset="utf8",
-        port=3306
-    )
+    cloudHost = os.environ.get("DB_HOST")
+    cloudUser = os.environ.get("DB_USER")
+    cloudPwd = os.environ.get("DB_PWD")
+    cloudName = os.environ.get("DB_NAME")
+    cloudPort = os.environ.get("DB_PORT")
+
+    cloudPort = int(cloudPort)
+    return pymysql.connect(host=cloudHost,
+                           user=cloudUser,
+                           password=cloudPwd,
+                           db=cloudName,
+                           charset='utf8',
+                           port=cloudPort)
