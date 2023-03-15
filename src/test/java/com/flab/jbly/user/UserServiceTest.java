c9e78cedb44613acfb877e228b73020841f65625
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-15 18:42:20 +0900

    feat : user login service test

--- src/test/java/com/flab/jbly/user/UserServiceTest.java
+++ src/test/java/com/flab/jbly/user/UserServiceTest.java
@@ -3,18 +3,26 @@ package com.flab.jbly.user;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.flab.jbly.application.user.LoginService;
 import com.flab.jbly.application.user.UserServiceImpl;
 import com.flab.jbly.infrastructure.exception.user.DoesNotExistUserException;
+import com.flab.jbly.infrastructure.exception.user.EncoderNoSuchAlgorithmException;
+import com.flab.jbly.presentation.user.request.LoginRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserServiceTest {
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private LoginService loginService;
 
     @DisplayName("사용자 회원가입 테스트")
     @Test
@@ -36,4 +44,34 @@ class UserServiceTest {
         assertThatThrownBy(() -> userService.getUserById(id)).isInstanceOf(
             DoesNotExistUserException.class);
     }
+
+    @DisplayName("사용자 로그인을 테스트합니다.")
+    @Test
+    public void loginTest() throws Exception {
+        // given
+        var signUp = UserSteps.AddUser();
+        userService.saveUser(signUp.toCommand()); // db 적재
+        var loginRequest = UserSteps.logInUser();
+
+        // when
+        var loginResult = loginService.login(loginRequest.toCommand());
+
+        // then
+        assertThat(loginResult.userId()).isEqualTo(signUp.getUserId());
+    }
+
+    @DisplayName("사용자 로그인 실패를 테스트합니다.")
+    @Test
+    public void loginFailTest() throws Exception {
+
+        var signUp = UserSteps.AddUser();
+        userService.saveUser(signUp.toCommand());
+        String userId = "yeun";
+        String pw = "!abcd1234";
+
+        var loginRequest = new LoginRequest(userId, pw);
+
+        assertThatThrownBy(() -> loginService.login(loginRequest.toCommand())).isInstanceOf(EncoderNoSuchAlgorithmException.class);
+    }
+
 }
