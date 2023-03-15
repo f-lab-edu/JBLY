c8cd5c4630aa0b23a63e586c2eaa02f7433ae229
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-03-15 18:40:26 +0900

    refactor : login Request

--- src/main/java/com/flab/jbly/presentation/user/request/LoginRequest.java
+++ src/main/java/com/flab/jbly/presentation/user/request/LoginRequest.java
@@ -2,6 +2,7 @@ package com.flab.jbly.presentation.user.request;
 
 import com.flab.jbly.application.user.command.LoginCommand;
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.util.Assert;
 
 public record LoginRequest(
 		@NotEmpty
@@ -10,7 +11,12 @@ public record LoginRequest(
 		String password
 ) {
 
-		public LoginCommand toCommand() {
+	public LoginRequest {
+		Assert.isTrue(userId.length() > 0, "로그인을 할 수 없습니다.");
+		Assert.isTrue(password.length() > 7, "비밀번호는 8~16자 영문, 숫자, 특수문자를 사용하세요.");
+	}
+
+	public LoginCommand toCommand() {
 				return new LoginCommand(
 						this.userId,
 						this.password
