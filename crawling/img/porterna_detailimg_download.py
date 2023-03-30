2c84df4ffa956260cc9b8f029a29298ae9e1e029
Park, Sojeong <creerari01@gmail.com>
2023-03-30 18:19:04 +0900

    setting: ssl

--- crawling/img/porterna_detailimg_download.py
+++ crawling/img/porterna_detailimg_download.py
@@ -17,7 +17,6 @@ from operator import length_hint
 from img import img_download
 import ssl
 
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
@@ -101,14 +100,14 @@ def porterna_img_downloader(target_url, item_type):
                             f.write(data)
 
                 elif item_type == product_types.TOP.name:
-                    with urllib.request.urlopen(link) as response:
+                    with urllib.request.urlopen(link, context=ctx) as response:
                         data = response.read()
                         md5hash = hashlib.md5(data).hexdigest()
                         md5hash = md5hash + ".jpg"
                         with open(path_folder_top + md5hash, 'wb') as f:
                             f.write(data)
                 elif item_type == product_types.BOTTOM.name:
-                    with urllib.request.urlopen(link) as response:
+                    with urllib.request.urlopen(link, context=ctx) as response:
                         data = response.read()
                         md5hash = hashlib.md5(data).hexdigest()
                         md5hash = md5hash + ".jpg"
@@ -116,7 +115,7 @@ def porterna_img_downloader(target_url, item_type):
                             f.write(data)
 
                 elif item_type == product_types.ACCESSORY.name:
-                    with urllib.request.urlopen(link) as response:
+                    with urllib.request.urlopen(link, context=ctx) as response:
                         data = response.read()
                         md5hash = hashlib.md5(data).hexdigest()
                         md5hash = md5hash + ".jpg"
@@ -124,7 +123,7 @@ def porterna_img_downloader(target_url, item_type):
                             f.write(data)
 
                 else:
-                    with urllib.request.urlopen(link) as response:
+                    with urllib.request.urlopen(link, context=ctx) as response:
                         data = response.read()
                         md5hash = hashlib.md5(data).hexdigest()
                         md5hash = md5hash + ".jpg"
