f749abf7a2031b825f4bc7f12e744d7b888bf7d7
Park, Sojeong <creerari01@gmail.com>
2023-04-04 01:09:08 +0900

    feat: morecherry image download

---
+++ crawling/img/img_crop.py
@@ -0,0 +1,53 @@
+import cv2
+from common.ProductTypes import product_types
+import hashlib
+import numpy as np
+
+path_folder_outwear = 'D:\\morecherry_outwear\\'
+path_folder_top = 'D:\\morecherry_top\\'
+path_folder_bottom = 'D:\\morecherry_bottom\\'
+path_folder_acc = 'D:\\morecherry_acc\\'
+path_folder_shoes = 'D:\\morecherry_shoes\\'
+
+def img_cropper(img, item_type):
+    # 이미지 파일을 불러옵니다.
+    image = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
+    # 첫 번째 이미지를 800x1000으로 자릅니다.
+    first_crop = image[:1000, :800].copy()
+    # first_crop = np.zeros((3, 3), dtype=np.float32)[::2, :]
+    # first_crop = np.ascontiguousarray(first_crop)
+    md5hash = hashlib.md5(first_crop).hexdigest() + ".jpg"
+    cv2.imwrite(path_folder_outwear + md5hash, first_crop)
+
+    # 이후 이미지들을 800x800으로 자릅니다.
+    num_crops = (image.shape[0] - 1000) // 800
+    crop_positions = [(i * 800 + 1000, (i + 1) * 800 + 1000) for i in range(num_crops)]
+    if (image.shape[0] - 1000) % 800 != 0:
+        crop_positions.append((num_crops * 800 + 1000, image.shape[0]))
+
+    # 이미지 파일에서 자른 이미지를 저장합니다.
+    for i, (start, end) in enumerate(crop_positions):
+        crop = image[start:end, :].copy()
+
+        # 왼쪽, 오른쪽 여백을 제거합니다.
+        gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        x, y, w, h = cv2.boundingRect(contours[0])
+        crop = crop[y:y + h, x:x + w]
+
+        # 중앙에 정렬된 이미지만 자릅니다.
+        height, width = crop.shape[:2]
+        if height > width:
+            diff = height - width
+            crop = crop[:, diff // 2:width + diff // 2]
+        elif width > height:
+            diff = width - height
+            crop = crop[diff // 2:height + diff // 2, :]
+        crop = np.zeros((3, 3), dtype=np.float32)[::2, :]
+        crop = np.ascontiguousarray(crop)
+        md5hash_2 = hashlib.md5(crop).hexdigest() + ".jpg"
+        cv2.imwrite(path_folder_outwear + md5hash_2, crop)
+
+    return None
---
+++ crawling/img/morecherry_img_download.py
@@ -0,0 +1,84 @@
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+from common.ProductTypes import product_types
+import urllib3
+import ssl
+import hashlib
+from img import img_crop
+from img_crop import img_cropper
+from urllib.parse import quote
+import numpy as np
+import cv2
+
+urllib3.disable_warnings()
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+main_url = "https://more-cherry.com"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+
+
+def find_last_page(target_url):
+    page_num = 1
+    header = {
+        'Referrer': target_url + str(page_num),
+        'user-agent': user_agent
+    }
+    response = requests.get(target_url + str(page_num), headers=header)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    find_ul = str(soup.find('a', 'last'))
+    match = re.search(r'\?page=(\d+)', find_ul)
+    page_num = int(match.group(1))
+    return page_num
+
+
+def detail_url_scraper(target_url):
+    href_list = []
+    for page_num in range(1, find_last_page(target_url) + 1):
+        header = {
+            'Referrer': target_url + str(page_num),
+            'user-agent': user_agent
+        }
+        response = requests.get(target_url + str(page_num), headers=header)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = soup.find_all('ul', {'class': 'prdList grid4'})
+        href_set = set()
+        for ul_tag in soup:
+            a_tags = ul_tag.find_all('a')
+            for a_tag in a_tags:
+                href = a_tag.get('href')
+                if href is not None:
+                    href_set.add(href)
+        href_list += list(href_set)
+    return href_list
+
+
+def morecherry_img_downloader(target_url, item_type):
+    href_list = detail_url_scraper(target_url)
+    for href in href_list:
+        detail_url = main_url + quote(str(href))
+        detail_header = {
+            'Referrer': detail_url,
+            'user-agent': user_agent
+        }
+        detail_response = requests.get(detail_url, headers=detail_header)
+        b_soup = BeautifulSoup(detail_response.text, 'html.parser')
+        detail_html = b_soup.find(id="prdDetail")
+        etc_img_tag = detail_html.find_all("img")[0]
+        clothes_img_tag = detail_html.find_all("img")[-1]
+
+        if item_type == product_types.OUTWEAR.name or item_type == product_types.TOP or item_type == product_types.BOTTOM:
+            img_url = main_url + clothes_img_tag['ec-data-src']
+            with urllib.request.urlopen(img_url) as response:
+                data = response.read()
+                img = np.asarray(bytearray(data), dtype=np.uint8)
+                img_cropper(img, item_type)
+        else:
+            img_url = main_url + etc_img_tag['ec-data-src']
+            with urllib.request.urlopen(img_url) as response:
+                data = response.read()
+                img = np.asarray(bytearray(data), dtype=np.uint8)
+                img_cropper(img, item_type)
+    return None
