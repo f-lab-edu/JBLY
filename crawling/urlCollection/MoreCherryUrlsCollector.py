0fd9417620c7312cde30409f648b171442a1d557
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-04-11 12:40:06 +0900

    refactor : morecherry site url parsing

--- crawling/urlCollection/MoreCherryUrlsCollector.py
+++ crawling/urlCollection/MoreCherryUrlsCollector.py
@@ -1,47 +1,46 @@
 import requests
-import threading
+import concurrent.futures
 from common.ProductTypes import product_types
 
-base_url = "https://more-cherry.com"
-crawling_module_name = "MoreCherrySiteCrawling"
-shop_name = "morecherry"
-find_key = '<ul class="prdList grid4">'
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, likeGecko) Chrome/109.0.0.0 Safari/537.36"
+base_url = "https://more-cherry.com"
 header = {
     'Referrer': base_url,
     'user-agent': user_agent
 }
 more_cherry_total_url = []
-urls = [
-    ("https://more-cherry.com/category/outwear/24/?page=", product_types.OUTWEAR.name),
-    ("https://more-cherry.com/category/top/25/?page=", product_types.TOP.name),
-    ("https://more-cherry.com/category/pants/26/?page=", product_types.BOTTOM.name),
-    ("https://more-cherry.com/category/accessory/28/?page=", product_types.ACCESSORY.name),
-    ("https://more-cherry.com/category/shoes/42/?page=", product_types.SHOES.name),
-]
-threads = []
 
 
-def fetch_url(url, item_type):
+def fetch_url(url):
+
+    crawling_module_name = "MoreCherrySiteCrawling"
+    find_key = '<ul class="prdList grid4">'
+
+    each_url, product_type = url
     page_number = 1
 
     while True:
-        temp_url = url + str(page_number)
+        temp_url = each_url + str(page_number)
         response = requests.get(temp_url, headers=header)
         page_number += 1
         if find_key in response.text:
-            more_cherry_total_url.append([crawling_module_name, response, item_type])
+            more_cherry_total_url.append([crawling_module_name, response, product_type])
         else:
             break
 
 def gather_urls():
-    for each_url in urls:
-        url, item_type = each_url
-        thread = threading.Thread(target=fetch_url, args=(url, item_type))
-        threads.append(thread)
-        thread.start()
 
-    for thread in threads:
-        thread.join()
+    shop_name = "morecherry"
+
+    urls = [
+        ("https://more-cherry.com/category/outwear/24/?page=", product_types.OUTWEAR.name),
+        ("https://more-cherry.com/category/top/25/?page=", product_types.TOP.name),
+        ("https://more-cherry.com/category/pants/26/?page=", product_types.BOTTOM.name),
+        ("https://more-cherry.com/category/accessory/28/?page=", product_types.ACCESSORY.name),
+        ("https://more-cherry.com/category/shoes/42/?page=", product_types.SHOES.name),
+    ]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        executor.map(fetch_url, urls)
 
     return [more_cherry_total_url, base_url, user_agent, shop_name]
