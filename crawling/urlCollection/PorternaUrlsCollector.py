87a75436e63938bd17398c2f87e00a5ffa81803d
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-04-11 12:57:10 +0900

    refactor : porterna site url parsing

--- crawling/urlCollection/PorternaUrlsCollector.py
+++ crawling/urlCollection/PorternaUrlsCollector.py
@@ -1,48 +1,45 @@
+import concurrent.futures
 import requests
-import threading
 from common.ProductTypes import product_types
 
-base_url = "https://porterna.com"
-crawling_module_name = "PorternaSiteCrawling"
-shop_name = "porterna"
-find_key = 'col  col20 floatleft xans-record-'
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+base_url = "https://porterna.com"
 header = {
     'Referrer': base_url,
     'user-agent': user_agent
 }
 porterna_total_url = []
-urls = [
-    ("https://porterna.com/product/list.html?cate_no=541&page=", product_types.OUTWEAR.name),
-    ("https://porterna.com/product/list.html?cate_no=789&page=", product_types.TOP.name),
-    ("https://porterna.com/product/list.html?cate_no=28&page=", product_types.BOTTOM.name),
-    ("https://porterna.com/product/list.html?cate_no=44&page=", product_types.ACCESSORY.name),
-    ("https://porterna.com/product/list.html?cate_no=79&page=", product_types.SHOES.name),
-]
-threads = []
 
 
-def fetch_url(url, item_type):
+def fetch_url(url):
+    crawling_module_name = "PorternaSiteCrawling"
+    find_key = 'col  col20 floatleft xans-record-'
+
+    each_url, product_type = url
     page_number = 1
 
     while True:
-        temp_url = url + str(page_number)
+        temp_url = each_url + str(page_number)
         response = requests.get(temp_url, headers=header)
         page_number += 1
         if find_key in response.text:
-            porterna_total_url.append([crawling_module_name, response, item_type])
+            porterna_total_url.append([crawling_module_name, response, product_type])
         else:
             break
 
 
 def gather_urls():
-    for each_url in urls:
-        url, item_type = each_url
-        thread = threading.Thread(target=fetch_url, args=(url, item_type))
-        threads.append(thread)
-        thread.start()
+    shop_name = "porterna"
 
-    for thread in threads:
-        thread.join()
+    urls = [
+        ("https://porterna.com/product/list.html?cate_no=541&page=", product_types.OUTWEAR.name),
+        ("https://porterna.com/product/list.html?cate_no=789&page=", product_types.TOP.name),
+        ("https://porterna.com/product/list.html?cate_no=28&page=", product_types.BOTTOM.name),
+        ("https://porterna.com/product/list.html?cate_no=44&page=", product_types.ACCESSORY.name),
+        ("https://porterna.com/product/list.html?cate_no=79&page=", product_types.SHOES.name),
+    ]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        executor.map(fetch_url, urls)
 
     return [porterna_total_url, base_url, user_agent, shop_name]
