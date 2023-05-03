b79c154af02e28860d2cfda4311c5525bc9a47de
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-05-03 20:04:22 +0900

    refactor : footer

--- frontend/reactfront/src/App.js
+++ frontend/reactfront/src/App.js
@@ -4,7 +4,7 @@ import Home from "./component/Home";
 import ProductListPage from "./component/page/ProductListPage";
 import SignUpPage from "./component/page/SignUpPage";
 import Header from "./component/Header";
-import Footer from "./component/Footer";
+import Footer from "./component/footer/Footer";
 import SearchBox from "./component/searchBox/SearchBox";
 
 const App = () => {
---
+++ frontend/reactfront/src/component/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+      <footer className={"footer-container"}>
+          <div className="footer-text">
+              F-Lab에서 진행하고 있는 JBLY 프로젝트입니다.
+              BY. SJ, YS
+          </div>
+      </footer>
+  )
+}
+
+export default Footer;
